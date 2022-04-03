05df7b96de0f4939e7fc10be4b9ea60298e27183
moonstal1506 <moonstal1506@gmail.com>
2022-04-04 00:44:40 +0900

    Test 실습

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    @DisplayName("Set 크기 확인")
+    public void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("Set 값 확인")
+    public void contains(int input){
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("Set 입력값 따른 결과값 확인")
+    public void contains2(int input,boolean expected){
+        boolean actualValue = numbers.contains(input);
+        assertEquals(expected, actualValue);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,61 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
+    @DisplayName("문자바꾸기")
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName(",로 쪼개기")
+    void split() {
+        String[] num = "1,2".split(",");
+        assertThat(num).contains("1");
+    }
+
+    @Test
+    @DisplayName("1을 ,로 쪼개면 1이다")
+    void split2() {
+        String[] num = "1".split(",");
+        assertThat(num).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("()제거하기")
+    void substring() {
+        String num = "(1,2)".substring(1,4);
+        assertThat(num).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정위치 문자 가져오기")
+    void charAt() {
+        char a = "abc".charAt(0);
+        assertThat(a).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("특정위치 문자 가져오기 예외 has")
+    public void testException() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(10);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("10");
+    }
+
+    @Test
+    @DisplayName("특정위치 문자 가져오기 예외 with")
+    public void testException2() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    "abc".charAt(10);
+                }).withMessageContaining("10");
     }
 }
