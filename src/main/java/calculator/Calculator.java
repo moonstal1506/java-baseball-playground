edb04d94ab1a338b841f6643a69f1e5c748da0f3
moonstal1506 <moonstal1506@gmail.com>
2022-04-09 15:25:59 +0900

    feat: 사칙연산

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,18 +4,34 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public String input(){
+    public String input() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
 
     }
 
-    public String[] split(String value){
+    public String[] split(String value) {
         return value.split(" ");
     }
 
-    public int makeInt(String value){
+    public int makeInt(String value) {
         return Integer.parseInt(value);
+    }
+
+    public int add(int result, int num) {
+        return result + num;
+    }
+
+    public int subtract(int result, int num) {
+        return result - num;
+    }
+
+    public int multiply(int result, int num) {
+        return result * num;
+    }
+
+    public int divide(int result, int num) {
+        return result / num;
     }
 
     public static void main(String[] args) {
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,22 +4,61 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    Calculator calculator =new Calculator();
+    Calculator calculator = new Calculator();
 
     @Test
     @DisplayName("공백으로 문자열 분리")
     void split() {
-        String[] values=calculator.split("2 + 3 * 4 / 2");
+        String[] values = calculator.split("2 + 3 * 4 / 2");
         assertThat(values[3]).isEqualTo("*");
     }
 
     @Test
-    void 문자열을_숫자로() {
-        int value=calculator.makeInt("2");
+    @DisplayName("문자열을 숫자로")
+    void makeInt() {
+        int value = calculator.makeInt("2");
         assertThat(value).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("더하기")
+    void add() {
+        int result = calculator.add(1, 2);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("빼기")
+    void subtract() {
+        int result = calculator.subtract(3, 1);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("곱하기")
+    void multiply() {
+        int result = calculator.multiply(2, 3);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("나누기")
+    void divide() {
+        int result = calculator.divide(4, 2);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("나누기 예외")
+    void divideException() {
+        assertThatThrownBy(() -> {
+            calculator.divide(4, 0);
+        }).isInstanceOf(ArithmeticException.class)
+                .hasMessageContaining("zero");
     }
 }
