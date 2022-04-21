65c2767df65ec9012153f76658401511ec86a9c0
moonstal1506 <moonstal1506@gmail.com>
2022-04-21 19:20:55 +0900

    feat: 문자열 입력 받기

---
+++ src/main/java/calculator/InputView.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static String input(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
