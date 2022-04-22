b684ba16404e75d01c62d8dad5a4c6484f680b51
moonstal1506 <moonstal1506@gmail.com>
2022-04-22 19:57:04 +0900

    feat: 숫자 입력받기

--- src/main/java/baseballgame/InputView.java
+++ src/main/java/baseballgame/InputView.java
@@ -4,4 +4,10 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private Scanner scanner = new Scanner(System.in);
+
+    public String input(){
+        System.out.print("숫자를 입력해 주세요 :");
+        return scanner.nextLine();
+    }
 }
