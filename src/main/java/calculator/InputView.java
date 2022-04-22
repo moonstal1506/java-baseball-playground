d8a83a215fa0dd8df1c2bf40f9e63e3b7ab7f6d3
moonstal1506 <moonstal1506@gmail.com>
2022-04-22 10:42:43 +0900

    feat: 값체크

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -13,6 +13,14 @@ public class InputView {
         return scanner.nextLine();
     }
 
+    public static void checkValidation(String input) {
+        if (isNullOrEmpty(input)) throw new IllegalArgumentException("값을 입력해야합니다.");
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.equals("");
+    }
+
     public static String[] spiltBySpace(String number) {
         return number.split(" ");
     }
