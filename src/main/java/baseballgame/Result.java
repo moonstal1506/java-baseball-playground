43c920135fee4b94a48691852fa1aa50815caf59
moonstal1506 <moonstal1506@gmail.com>
2022-04-23 15:21:31 +0900

    feat: 볼 개수 카운트

--- src/main/java/baseballgame/Result.java
+++ src/main/java/baseballgame/Result.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Result {
 
     private int strike=0;
+    private int ball=0;
 
     public int countStrike(List<Integer> computerBalls,List<Integer> userBalls){
         for(int i=0;i<computerBalls.size();i++){
@@ -15,4 +16,14 @@ public class Result {
         return strike;
     }
 
+    public int countBall(List<Integer> computerBalls,List<Integer> userBalls){
+        int equalBallNumber=0;
+        for(int i=0;i<computerBalls.size();i++){
+            if(computerBalls.contains(userBalls.get(i))){
+                equalBallNumber++;
+            }
+        }
+        this.ball=equalBallNumber-strike;
+        return ball;
+    }
 }
--- src/test/java/baseballgame/ResultTest.java
+++ src/test/java/baseballgame/ResultTest.java
@@ -1,5 +1,6 @@
 package baseballgame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -7,14 +8,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ResultTest {
 
+    private Result result;
+    private UserBalls userBalls;
+
+    @BeforeEach
+    void setUp() {
+        result = new Result();
+        userBalls = new UserBalls("123");
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"123:3", "124:2", "145:1", "456:0"}, delimiter = ':')
     void 스트라이크개수(String input, int expected) {
-        Result result = new Result();
-        UserBalls userBalls = new UserBalls("123");
+
         UserBalls userBalls2 = new UserBalls(input);
 
-        int strike=result.countStrike(userBalls.getBalls(),userBalls2.getBalls());
+        int strike = result.countStrike(userBalls.getBalls(), userBalls2.getBalls());
         assertThat(strike).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:3", "234:2", "345:1", "456:0"}, delimiter = ':')
+    void 볼개수(String input, int expected) {
+        UserBalls userBalls2 = new UserBalls(input);
+
+        int ball = result.countBall(userBalls.getBalls(), userBalls2.getBalls());
+        assertThat(ball).isEqualTo(expected);
     }
 }
