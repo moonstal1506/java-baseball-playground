398b9985546785f6f5dddea09ba6ded4a4e85b5a
moonstal1506 <moonstal1506@gmail.com>
2022-04-24 14:11:18 +0900

    test: report 테스트 추가

--- src/test/java/baseballgame/ResultTest.java
+++ src/test/java/baseballgame/ResultTest.java
@@ -7,9 +7,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ResultTest {
 
     @Test
-    void 결과판단() {
+    void 낫싱() {
+        Result result = new Result(0, 0);
+        assertThat(result.report()).isEqualTo("낫싱");
+    }
+
+    @Test
+    void 스트라이크3() {
         Result result = new Result(3, 3);
         assertThat(result.report()).isEqualTo("3스트라이크");
+    }
+
+    @Test
+    void 볼3() {
+        Result result = new Result(3, 0);
+        assertThat(result.report()).isEqualTo("3볼");
+    }
+
+    @Test
+    void 스트라이크1볼2() {
+        Result result = new Result(3, 1);
+        assertThat(result.report()).isEqualTo("1스트라이크 2볼");
     }
 
 }
