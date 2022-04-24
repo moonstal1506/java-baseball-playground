6d1cb87a62de3aeb216f21b731817480c5b992f9
moonstal1506 <moonstal1506@gmail.com>
2022-04-24 13:43:56 +0900

    fix: 숫자 다 맞췄을 때만 문장출력

--- src/main/java/baseballgame/ResultView.java
+++ src/main/java/baseballgame/ResultView.java
@@ -4,8 +4,8 @@ public class ResultView {
 
     public void printResult(String result) {
         System.out.println(result);
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
+        if(result.equals("3스트라이크")){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
-
 }
